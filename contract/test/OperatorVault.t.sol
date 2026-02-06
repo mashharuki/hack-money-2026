1dce6bfd5739a321db18c06732dd34202c6bd39e
mashharuki <avp.104.106.107@ae.auone-net.jp>
2026-02-06 23:28:24 +0900

    カバレッジを上げるためにテストコードを追加

--- contract/test/OperatorVault.t.sol
+++ contract/test/OperatorVault.t.sol
@@ -68,6 +68,47 @@ contract OperatorVaultTest is Test {
         assertEq(usdc.balanceOf(owner), amount);
     }
 
+    function test_DepositUSDC_RevertsOnZeroAmount() public {
+        vm.expectRevert(bytes("OperatorVault: amount is zero"));
+        vault.depositUSDC(0);
+    }
+
+    function test_DepositUSDC_RevertsOnTransferFailure() public {
+        FailingUSDC failing = new FailingUSDC();
+        OperatorVault failingVault = new OperatorVault(address(failing), owner);
+        failing.setFailTransferFrom(true);
+
+        vm.prank(user);
+        failing.approve(address(failingVault), 1);
+
+        vm.expectRevert(bytes("OperatorVault: transfer failed"));
+        vm.prank(user);
+        failingVault.depositUSDC(1);
+    }
+
+    function test_Withdraw_RevertsOnZeroAmount() public {
+        vm.expectRevert(bytes("OperatorVault: amount is zero"));
+        vm.prank(owner);
+        vault.withdraw(0);
+    }
+
+    function test_Withdraw_RevertsOnInsufficientBalance() public {
+        vm.expectRevert(bytes("OperatorVault: insufficient balance"));
+        vm.prank(owner);
+        vault.withdraw(1);
+    }
+
+    function test_Withdraw_RevertsOnTransferFailure() public {
+        FailingUSDC failing = new FailingUSDC();
+        OperatorVault failingVault = new OperatorVault(address(failing), owner);
+        failing.setFailTransfer(true);
+        failing.mint(address(failingVault), 10);
+
+        vm.expectRevert(bytes("OperatorVault: transfer failed"));
+        vm.prank(owner);
+        failingVault.withdraw(1);
+    }
+
     function test_DepositUSDC_ReentrancyGuard() public {
         ReentrantUSDC reentrant = new ReentrantUSDC();
         OperatorVault reentrantVault = new OperatorVault(address(reentrant), owner);
@@ -97,6 +138,39 @@ contract OperatorVaultTest is Test {
         vm.expectRevert(ReentrancyGuard.ReentrancyGuardReentrantCall.selector);
         vm.prank(owner);
         reentrantVault.withdraw(amount);
+    }
+}
+
+contract FailingUSDC is ERC20 {
+    bool private failTransfer;
+    bool private failTransferFrom;
+
+    constructor() ERC20("Failing USDC", "fUSDC") {}
+
+    function setFailTransfer(bool value) external {
+        failTransfer = value;
+    }
+
+    function setFailTransferFrom(bool value) external {
+        failTransferFrom = value;
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        if (failTransfer) {
+            return false;
+        }
+        return super.transfer(to, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
+        if (failTransferFrom) {
+            return false;
+        }
+        return super.transferFrom(from, to, amount);
     }
 }
 
